771d8c657f221c01b38b9fa775ea6b5d8134c775
MatthewMerrill <MattMerr47@gmail.com>
2017-09-23 20:41:15 -0700

    Notchify v1.0

---
+++ notchify.js
@@ -0,0 +1,12 @@
+(function(){
+  const div = document.createElement('div');
+  div.style.position = 'absolute';
+  div.style.top = '0';
+  div.style.left = '40%';
+  div.style.width = '20%';
+  div.style.minHeight = '50px';
+  div.style.height = '10%';
+  div.style.background = 'black';
+  div.style.zIndex = 999999;
+  document.body.appendChild(div);
+})();
